a2cddb01c33be8d215766daec7ac72850283553a
masao5784 <73886478+masao5784@users.noreply.github.com>
2020-11-03 21:48:38 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,9 @@
+
 var myBirthTime = new Date(1999,04,7,9,25);
 function updateParagraph(){
     var now = new Date();
-    var seconds = (now.getTime()-myBirthTime.getTime())/1000;
-    document.getElementById('birth-time').innerText='生まれてから'+seconds+'秒経過。';
+    var seconds = (now.getTime()-myBirthTime.getTime());
+    var myBirthTime = Math.floor(seconds/(1000*60*60*24));
+    document.getElementById('birth-time').InnerText='生まれてから'+(myBirthTime+1)+'日目になります'
 }
 setInterval(updateParagraph,50);
